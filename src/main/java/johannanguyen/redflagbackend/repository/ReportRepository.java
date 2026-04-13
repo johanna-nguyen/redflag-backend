d8983cb878c46b448e54220047ff1e2a01d041f1
Johanna Nguyen <johannanguyenfr@gmail.com>
2026-04-13 15:45:41 +0200

    Update report repository

--- src/main/java/johannanguyen/redflagbackend/repository/ReportRepository.java
+++ src/main/java/johannanguyen/redflagbackend/repository/ReportRepository.java
@@ -1,9 +1,10 @@
 package johannanguyen.redflagbackend.repository;
 
 import johannanguyen.redflagbackend.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ReportRepository extends JpaRepository<Report, Long>{
+public interface ReportRepository extends JpaRepository<Report, Long> {
     List<Report> findByFirstNameContainingIgnoreCase(String name);
 }
